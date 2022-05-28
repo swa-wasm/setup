197390665a2835d9a7f4312c3205d163a18db9fb
Lukas Panni <lukas-panni@web.de>
2022-05-28 16:51:23 +0200

    Fix scripts

--- setup.ps1
+++ setup.ps1
@@ -8,8 +8,10 @@ Foreach ($prg in $programs){
     winget install --exact --silent $prg
 }
 
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") # sicherstellen, dass code im Pfad ist
 
 code --install-extension ms-vscode-remote.vscode-remote-extensionpack
 
 git clone https://github.com/swa-wasm/wasm-playground                             # Projekt-Repository klonen
 code wasm-playground                                                              # VS Code öffnen
+# Bei erster Installation von Docker-Desktop ist vermutlich ein Neustart/Logout notwendig
--- wsl_setup.ps1
+++ wsl_setup.ps1
@@ -1,4 +1,5 @@
 wsl --install
+mkdir C:\temp\
 Invoke-WebRequest -Uri "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" -Outfile C:\temp\wsl_update.msi
 Start-Process msiexec.exe -Wait -ArgumentList '/I C:\temp\wsl_update.msi /quiet'
-Restart-Computer -Wait
+Restart-Computer
